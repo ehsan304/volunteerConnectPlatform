6733bddcdd5c9519fe7b7fd25c99472db868caba
ehsan304 <muhammadehsan304@gmail.com>
2025-09-12 19:33:52 +0500

    Update API URL and vercel config

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
     
-    baseURL: "https://volunteer-platform-backend-ztfm.onrender.com/api",
+    baseURL: import.meta.env.VITE_API_URL,
     withCredentials: true, 
 });
 
