c24f837106281700c41502720ee83dca973609f2
ehsan304 <muhammadehsan304@gmail.com>
2025-09-12 19:12:40 +0500

    add backend's render link to the frontend API file

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5001/api",
+    
+    baseURL: "https://volunteer-platform-backend-ztfm.onrender.com/api",
     withCredentials: true, 
 });
 
